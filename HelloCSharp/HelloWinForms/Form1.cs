e1c87494aa3f272f2aaec30b6b6da30880de3d8a
GiaHuyhe187192 <huynghe187192@fpt.edu.vn>
2026-01-28 14:44:10 +0700

    Add goodbye button

--- HelloCSharp/HelloWinForms/Form1.cs
+++ HelloCSharp/HelloWinForms/Form1.cs
@@ -9,17 +9,22 @@ namespace HelloWinForms
         {
             InitializeComponent();
 
-            
+       
             Button btnHello = new Button();
             btnHello.Text = "Xin chào";
             btnHello.Top = 30;
             btnHello.Left = 30;
             btnHello.Click += BtnHello_Click;
 
-          
+           
+            Button btnBye = new Button();
+            btnBye.Text = "Tạm biệt";
+            btnBye.Top = 70;  
+            btnBye.Left = 30;
+            btnBye.Click += BtnBye_Click;
 
             this.Controls.Add(btnHello);
-           
+            this.Controls.Add(btnBye);
         }
 
         private void BtnHello_Click(object sender, EventArgs e)
@@ -27,6 +32,9 @@ namespace HelloWinForms
             MessageBox.Show("Xin chào 👋");
         }
 
-    
+        private void BtnBye_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tạm biệt 👋");
+        }
     }
 }
